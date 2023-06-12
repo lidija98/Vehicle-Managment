05a834fbd57c55cdc178d015f5ceec343bff3d94
Lidija Čutura <93079523+lidija98@users.noreply.github.com>
2023-06-12 17:21:44 +0200

    Initial commit

---
+++ Project.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
