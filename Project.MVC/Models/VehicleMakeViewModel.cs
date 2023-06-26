31b5ec1b3b7c11e47253c197c7cadf6e0d1cbfb5
Lidija Čutura <93079523+lidija98@users.noreply.github.com>
2023-06-26 20:55:50 +0200

    Add view models

---
+++ Project.MVC/Models/VehicleMakeViewModel.cs
@@ -0,0 +1,9 @@
+﻿public class VehicleMakeViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Abrv { get; set; }
+
+    // Example of a calculated property
+    public string FullName => $"{Name} ({Abrv})";
+}
---
+++ Project.MVC/Models/VehicleModelViewModel.cs
@@ -0,0 +1,11 @@
+﻿public class VehicleModelViewModel
+{
+    public int Id { get; set; }
+    public int MakeId { get; set; }
+    public string Name { get; set; }
+    public string Abrv { get; set; }
+    public string MakeName { get; set; }
+
+    // Example of a calculated property
+    public string DisplayText => $"{MakeName} - {Name} ({Abrv})";
+}
